b20f9efc10ee0b187217414bf920eaf3ecb081b6
Fosco <analytics@1lcapital.com>
2024-05-03 21:10:52 +0200

    work on plotting VIX term structure

--- routines/generate_VIX_term_structure.py
+++ routines/generate_VIX_term_structure.py
@@ -4,95 +4,21 @@ import pandas as pd
 import numpy as np
 from scipy import interpolate
 import matplotlib.pyplot as plt
+from utils.query_mongoDB_functions import *
+import plotly.graph_objs as go
 
 url = "mongodb+srv://foscoa:Lsw0r4KyI0rlq8YH@cluster0.vu31vch.mongodb.net/"
-
 client = pymongo.MongoClient(url)
-
-def q_TS_ETFs(tickers, start, end, param, collection):
-
-    # Define start and end dates in datetime format
-    start_date = datetime.strptime(start, "%Y-%m-%d")  # start date
-    end_date = datetime.strptime(end, "%Y-%m-%d")  # end date
-
-    # Initialize empty df to return
-    data_to_return = pd.DataFrame()
-
-    # loop on all tickers
-    for tck in tickers:
-
-        # Define the query to retrieve opening prices within the specified date range
-        query = {
-            "Date": {"$gte": start_date, "$lte": end_date},  # Filter documents by date range
-            param: {"$exists": True},  # Filter documents that have the "Open" field
-            "Ticker": tck
-        }
-
-        # Project only the "Open" and "Date" fields
-        projection = {"_id": 0, param: 1, "Date": 1}
-
-        # Execute the query
-        cursor = collection.find(query, projection).sort("Date", pymongo.ASCENDING)
-
-        # Iterate over the cursor to get params and corresponding dates
-        opening_prices = []
-        dates = []
-        for document in cursor:
-            opening_prices.append(document[param])
-            dates.append(document["Date"])
-
-        data_to_return = pd.merge(data_to_return,
-                            pd.DataFrame(data=opening_prices, index=dates),
-                            left_index=True, right_index=True, how='outer')
-
-    data_to_return.columns = [tck.replace('^', '') for tck in tickers]
-
-    return data_to_return
-
-def q_TS_VIX_futures(contracts, start, end, param, collection):
-
-    # Define start and end dates in datetime format
-    start_date = datetime.strptime(start, "%Y-%m-%d")  # start date
-    end_date = datetime.strptime(end, "%Y-%m-%d")  # end date
-
-    # Define the query to retrieve opening prices within the specified date range
-    query = {
-        "Trade Date": {"$gte": start_date, "$lte": end_date},  # Filter documents by date range
-        param: {"$exists": True},  # Filter documents that have the "Open" field
-        "Futures": {"$in": contracts}
-    }
-
-    # Project only the "Open" and "Date" fields
-    projection = {"_id": 0, param: 1, "Trade Date": 1, 'Futures': 1}
-
-    # Execute the query
-    cursor = collection.find(query, projection)
-
-    # Create DataFrame
-    df = pd.DataFrame(list(cursor))
-
-    # Set 'Trade Date' as index
-    df.set_index('Trade Date', inplace=True)
-
-    # Pivot DataFrame to have 'Futures' as columns
-    data_to_return = df.pivot(columns='Futures', values=param)
-
-    # fill nan with 0
-    data_to_return.fillna(value=0, inplace=True)
-
-    return data_to_return
-
 
 # QUERY ETFs ----
 
 db = client['ETFs']
 collection = db['Yahoo_Finance']
 
-print(client.list_database_names())
 
 start = '2013-01-01'
 end = '2024-03-31'
-tickers = ['SPY', 'VIXY', 'SVXY', '^VIX']
+tickers = ['^VIX']
 param = "Open"
 
 ts = q_TS_ETFs(tickers=tickers,
@@ -170,12 +96,46 @@ for today in open.index[open.index >= ts.index[0]]:
 
         interpol_dt = today.to_datetime64() + [np.timedelta64((k+1)*30, 'D') for k in np.arange(8)]
 
-        # # plot
-        # fig, ax = plt.subplots(figsize=(6.5, 4))
-        # ax.plot(x, y, 'o', label='data')
-        # ax.plot(dt, cs(dt), label="S")
-        # ax.plot(interpol_dt, cs(interpol_dt), 'o', label="interpol")
-        # plt.show()
+        # plot
+        fig, ax = plt.subplots(figsize=(6.5, 4))
+        ax.plot(x, y, 'o', label='data')
+        ax.plot(dt, cs(dt), label="S")
+        ax.plot(interpol_dt, cs(interpol_dt), 'o', label="interpol")
+        plt.show()
+
+
+        trace2 = go.Scatter(
+            x=x,
+            y=y,
+            mode='markers',
+            name='Traded Contracts',
+            marker=dict(size=12,
+                        color='darkred'),
+        )
+        trace1 = go.Scatter(
+            x=dt,
+            y=cs(dt),
+            mode='lines',
+            name='Fit',
+            opacity=0.5
+        )
+
+        trace3 = go.Scatter(
+            x=interpol_dt,
+            y=cs(interpol_dt),
+            mode='markers',
+            name='Interpolated Points',
+            marker=dict(size=12,
+                        symbol='square-open',
+                        color='midnightblue'),
+        )
+
+        data = [trace1, trace2, trace3]
+
+        fig = go.Figure(data=data)
+        fig.show()
+
+
 
         count = 1
         for date in interpol_dt[1:]:
