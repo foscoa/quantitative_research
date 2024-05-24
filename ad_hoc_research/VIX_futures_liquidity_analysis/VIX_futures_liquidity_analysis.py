66f14e3073c2bd99f76c47392e68b4d487594a76
Fosco <analytics@1lcapital.com>
2024-05-24 22:04:39 +0200

    friday evening commitment

--- ad_hoc_research/VIX_futures_liquidity_analysis/VIX_futures_liquidity_analysis.py
+++ ad_hoc_research/VIX_futures_liquidity_analysis/VIX_futures_liquidity_analysis.py
@@ -1,74 +1,106 @@
 import pandas as pd
 from utils.query_mongoDB_functions import *
+import plotly.io as pio
 import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+# display plot on the browser
+pio.renderers.default = "browser"
 
 # database connection
 url = "mongodb+srv://foscoa:Lsw0r4KyI0rlq8YH@cluster0.vu31vch.mongodb.net/"
 client = pymongo.MongoClient(url)
 
-# pull VIX futures data ---------------------------------------------------------------------------------------
 
-db = client.Listed_Futures
-collection = db.CBOE_VIX_Futures_monthly
+def pull_liquidity_data(client, start, end):
+    # pull VIX futures data ---------------------------------------------------------------------------------------
+    db = client.Listed_Futures
+    collection = db.CBOE_VIX_Futures_monthly
 
-start = '2013-01-02'
-end = '2024-03-31'
+    # Project only the "Open" and "Date" fields
+    projection = {"_id": 0, "Futures": 1, 'Expiry': 1}
 
-# Project only the "Open" and "Date" fields
-projection = {"_id": 0, "Futures": 1, 'Expiry': 1}
+    # Execute the query
+    futures_map = pd.DataFrame(list(collection.find({}, projection))).drop_duplicates().sort_values(by='Expiry')
 
-# Execute the query
-futures_map = pd.DataFrame(list(collection.find({}, projection))).drop_duplicates().sort_values(by='Expiry')
+    contracts = list(futures_map.Futures)
 
-contracts = list(futures_map.Futures)
+    # Total Volume
+    tot_vol = q_TS_VIX_futures(contracts=contracts,
+                               start=start,
+                               end=end,
+                               param='Total Volume',
+                               collection=collection)
 
-# Total Volume
-tot_vol = q_TS_VIX_futures(contracts=contracts,
-                           start=start,
-                           end=end,
-                           param='Total Volume',
-                           collection=collection)
+    tot_vol = tot_vol[[fut for fut in list(futures_map.Futures) if fut in tot_vol.columns]]  # order by maturity
 
-tot_vol = tot_vol[list(futures_map.Futures)]  # order by maturity
+    # Expiry
+    param = 'Expiry'
 
-# Expiry
-param = 'Expiry'
+    expiry = q_TS_VIX_futures(contracts=contracts,
+                            start=start,
+                            end=end,
+                            param=param,
+                            collection=collection)
 
-expiry = q_TS_VIX_futures(contracts=contracts,
-                        start=start,
-                        end=end,
-                        param=param,
-                        collection=collection)
+    expiry = expiry[[fut for fut in list(futures_map.Futures) if fut in expiry.columns]]  # order by maturity
 
-expiry = expiry[list(futures_map.Futures)]  # order by maturity
+    # calculate days left to expiration
+    days_left = expiry.apply(lambda column: (column - column.index))
 
-# calculate days left to expiration
-days_left = expiry.apply(lambda column: (column - column.index).days)
+    # save days left and
+    liquidity = [
+        [days_left[i].loc[j].days, tot_vol[i].loc[j]]
+        for i in tot_vol.columns
+        for j in tot_vol.index
+        if not pd.isna(days_left[i].loc[j])
+    ]
 
-empty_list = list()
-for i in tot_vol.columns:
-    for j in tot_vol.index:
-        if not pd.isna(days_left[i].loc[j]):
-            empty_list.append([days_left[i].loc[j].days, tot_vol[i].loc[j]])
+    liquidity = pd.DataFrame(liquidity, columns=['DTM', 'Volume']) # create dataframe
+    liquidity = liquidity[liquidity.DTM != 0] # contracts maturing in the same day
+    liquidity = liquidity.groupby(['DTM']).median() # calculate median
+    liquidity['Days to Expiry'] = liquidity.index
+    liquidity.rename(columns={'Volume': 'Median Volume'}, inplace=True) # enaming volume column
 
-
-liquidity = pd.DataFrame(empty_list, columns=['DTM', 'Volume'])
-
-liquidity = liquidity[liquidity.DTM != 0] # remove DTM = 0
-
-liquidity = liquidity.groupby(['DTM']).median()
-
-liquidity['Days to Expiry'] = liquidity.index
-
-# Renaming a single column
-liquidity.rename(columns={'Volume': 'Median Volume'}, inplace=True)
-
+    return liquidity
 
 # Scatter plot with Plotly Express
-fig = px.scatter(liquidity,
+end = '2024-03-31'
+
+liquidity = pull_liquidity_data(client=client, start='2013-01-01', end=end)
+fig1 = px.scatter(liquidity,
                  x='Days to Expiry',
                  y='Median Volume',
                  title='Median volume for given days to expiry')
+period1 = '2013-01-01' + ' / ' + end
 
-# Show the plot
-fig.show()
+liquidity = pull_liquidity_data(client=client, start='2020-01-01', end=end)
+fig2 = px.scatter(liquidity,
+                 x='Days to Expiry',
+                 y='Median Volume',
+                 title='Median volume for given days to expiry')
+period2 = '2020-01-01' + ' / ' + end
+
+# Create a subplot
+fig = make_subplots(rows=2, cols=1, subplot_titles=(period1, period2))
+
+# Add traces from fig1 to the first subplot
+for trace in fig1.data:
+    fig.add_trace(trace, row=1, col=1)
+
+# Add traces from fig2 to the second subplot
+for trace in fig2.data:
+    fig.add_trace(trace, row=2, col=1)
+
+
+# Set axis limits for the first subplot
+fig.update_yaxes(range=[0, 120000], row=1, col=1)  # Set y-axis limit
+fig.update_yaxes(range=[0, 120000], row=2, col=1)    # Set y-axis limit
+
+
+# Save the figure
+out_dir = "C:\\Users\\Fosco\\Desktop\\quantitative_research\\ad_hoc_research\\VIX_futures_liquidity_analysis\\"
+
+# fig.write_image(out_dir + "VX_liquidity.eps", width=1920, height=1080)
+
